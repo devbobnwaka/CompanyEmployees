0087ddc182cfcca37069859e71b246a4184cf58c
Chibueze Bob Nwaka <nwakachibueze@gmail.com>
2024-06-21 16:04:34 +0100

    using automapper

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -13,8 +13,12 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
